7b5df42eb23ea75e643574b73d1abddfdeffda65
reoil <imidg3001@yandex.ru>
2019-11-30 14:22:13 +0300

    [FIX]Fix text crash

--- changellenge/wsgi.py
+++ changellenge/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Challange project.
+WSGI config for changellenge project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Challange.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'changellenge.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Challange.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'changellenge.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
