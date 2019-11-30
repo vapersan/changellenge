9fd00eafe3aa2765357c86e89b55f9ddb75f0288
reoil <imidg3001@yandex.ru>
2019-11-30 16:48:42 +0300

    [ADD]Added database model in django

--- changellenge_web/admin.py
+++ changellenge_web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Services
+
 
 # Register your models here.
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    pass
--- changellenge_web/models.py
+++ changellenge_web/models.py
@@ -1,3 +1,97 @@
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
+
 # Create your models here.
+class Links(models.Model):
+    class Meta:
+        verbose_name = _('Link')
+        verbose_name_plural = _('Links')
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name=_('Name')
+    )
+
+    url = models.URLField(
+        verbose_name=_('Url')
+    )
+
+
+class Services(models.Model):
+    class Meta:
+        verbose_name = _('Service')
+        verbose_name_plural = _('Services')
+
+    service_status = (
+        ('IDE', _('Idea')),
+        ('IDV', _('In dev')),
+        ('DEB', _('Debug')),
+        ('COO', _('Сooked')),
+    )
+    name = models.CharField(
+        max_length=255,
+        verbose_name=_('Name')
+    )
+
+    about = models.TextField(
+        max_length=50000,
+        verbose_name=_('About')
+    )
+
+    authors = models.ManyToManyField(
+        to=get_user_model(),
+        verbose_name=_('Authors')
+    )
+
+    status = models.CharField(
+        max_length=3,
+        verbose_name=_('Status'),
+        choices=service_status
+    )
+
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('Date added')
+    )
+
+    date_release = models.DateTimeField(
+        blank=True,
+        default=None,
+        null=True,
+        verbose_name=_('Date added')
+    )
+
+    stars = models.PositiveIntegerField(
+        default=0,
+        verbose_name=_('Stars')
+    )
+
+    links = models.ManyToManyField(
+        to=Links,
+        verbose_name=_('Links')
+    )
+
+
+class ServicesRelation(models.Model):
+    """
+    Service relation parent
+    """
+
+    class Meta:
+        verbose_name = _('Service relation')
+        verbose_name_plural = _('Service relations')
+
+    children = models.OneToOneField(
+        to=Services,
+        on_delete=models.CASCADE,
+        verbose_name=_('Children'),
+        related_name='Children'
+    )
+
+    parents = models.ManyToManyField(
+        to=Services,
+        verbose_name=_('Parents'),
+        related_name='Parents'
+    )
