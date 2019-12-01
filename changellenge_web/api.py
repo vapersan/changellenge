227169f72e230c32749481ba0a148e59764ef419
reoil <imidg3001@yandex.ru>
2019-12-01 06:53:57 +0300

    [UPDATE]Added search engine

--- changellenge_web/api.py
+++ changellenge_web/api.py
@@ -1,22 +1,29 @@
+from django.db.models import Q
 from django.http import JsonResponse, Http404, HttpResponse
+from django.template.defaultfilters import truncatechars_html
 from martor.templatetags.martortags import safe_markdown
 
 from . import models
 
 
 def get_services(request):
+    q = request.GET.get('q', '')
     return JsonResponse(
         [
             {
                 'id': x.id,
                 'name': x.name,
                 'about': x.about,
+                'about_html': truncatechars_html(safe_markdown(x.about), 100),
                 'authors': [{'first_name': y.first_name, 'last_name': y.last_name} for y in x.authors.all()],
                 'status': x.status,
+                'status_name': [d for d in models.Services.service_status if d[0] == x.status][0][1],
                 'date_created': x.date_created,
                 'stars': x.stars,
                 'parents': [y.id for y in x.parents().all()],
-            } for x in models.Services.objects.all()], safe=False, json_dumps_params={'ensure_ascii': False}
+            } for x in models.Services.objects.filter(Q(about__contains=q) | Q(name__contains=q))],
+        safe=False,
+        json_dumps_params={'ensure_ascii': False}
     )
 
 
@@ -32,6 +39,7 @@ def get_service(request, id_):
         'about_html': safe_markdown(x.about),
         'authors': [{'first_name': y.first_name, 'last_name': y.last_name} for y in x.authors.all()],
         'status': x.status,
+        'status_name': [d for d in models.Services.service_status if d[0] == x.status][0][1],
         'date_created': x.date_created,
         'stars': x.stars,
         'parents': [y.id for y in x.parents().all()],
--- changellenge_web/urls.py
+++ changellenge_web/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add-service/', views.add_service, name='add-service'),
-    path('liven_test/', views.liven_test),
     path('', views.index, name='index'),
     path('services/', views.services, name='services'),
 ]
